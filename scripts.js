e007dbad640701990452df7cdb1cce64174a1549
JasonLazzuri <Jason.Lazzuri@gmail.com>
2017-04-05 20:15:19 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+function name(userInput) {
+  var myArray = [];
+    for(var i = 1; i <= userInput; i++){
+    if (i % 15 === 0){
+      myArray.push("ping-pong");
+    }else if (i % 5 === 0){
+      myArray.push("pong");
+    }else if (i % 3 === 0){
+        myArray.push("ping");
+    }else {
+      myArray.push(i);
+    }
+  }return myArray;
+}
+
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+
+    var userInput = $("#userText").val();
+    var result = name(userInput);
+
+    $('#results').text(result).show();
+  });
+});
